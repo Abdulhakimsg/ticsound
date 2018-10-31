6e5b695422461faecf4fb5534e3848b6a9bc9447
wenosaurus <wen.mcjawn@gmail.com>
2018-10-31 10:16:38 +0800

    Added CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,6 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: 'wen@gmail.com', username: 'wenvo', password: '123456')
+
+Event.create(name: "hakim", user_id: User.first.id)
