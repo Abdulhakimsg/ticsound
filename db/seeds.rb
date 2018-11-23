31a9a94e6c874151ffb4d20b3ec7751871dd8b30
abdulhakimsg <abdulhakimchi@gmail.com>
2018-11-23 11:17:16 +0800

    changed data date

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,8 @@ Event.create(name: "Oddisee presented by Collective Minds x Kilo Lounge",
              address: "21 Tanjong Pagar Road",
              ticket_url: "https://ticketing.igo.events/e/304/oddisee-presented-by-collective-minds-x-kilo-lounge",
              description: "The son of Sudanese and American parents, Oddisee was born and raised in Washington DC, spending hot summers in Khartoum learning Arabic and swimming in the Nile. Oddisee took his first steps as an MC producer in the analog basement studio of his legendary neighbor, Garry Shider (Parliament Funkadelic) and has since performed hundreds of shows around the globe. ",
-             starttime: DateTime.strptime("11/14/2018 21:00", "%m/%d/%Y %H:%M"),
-             endtime: DateTime.strptime("11/15/2018 01:00", "%m/%d/%Y %H:%M"),
+             starttime: DateTime.strptime("12/14/2018 21:00", "%m/%d/%Y %H:%M"),
+             endtime: DateTime.strptime("12/15/2018 01:00", "%m/%d/%Y %H:%M"),
              image: Rails.root.join("public/uploads/event/image/1/oddisee.jpg").open,
              user_id: 1)
 
@@ -30,8 +30,8 @@ Event.create(name: "BlackKajiXtra - Acid Mothers Temple & The Melting Paraiso UF
              Acid Mothers Temple & The Melting Paraiso U.F.O.
              Opening Set by DJ Ujikaji
              In conjunction with the launch of Trails to the Cosmic Vibrations LP (AMT split with The Observatory)",
-             starttime: DateTime.strptime("11/22/2018 19:30", "%m/%d/%Y %H:%M"),
-             endtime: DateTime.strptime("11/22/2018 22:30", "%m/%d/%Y %H:%M"),
+             starttime: DateTime.strptime("12/22/2018 19:30", "%m/%d/%Y %H:%M"),
+             endtime: DateTime.strptime("12/22/2018 22:30", "%m/%d/%Y %H:%M"),
              image: Rails.root.join("public/uploads/event/image/2/42457886_2148839248492442_5575776206374043648_o.jpg").open,
              user_id: 2)
 
@@ -39,8 +39,8 @@ Event.create(name: "Gimme Shelter",
              venue: "Intermission Bar - at The Projector",
              address: "6001 Beach Road",
              description: "Gimme Shelter parties are about the people and the music. Bringing back forgotten dance classics and digging for undiscovered gems, we evoke a timeless house party vibe, with DJs throwing records on the turntables back to back, and where the only rule is EVERYBODY DANCE!",
-             starttime: DateTime.strptime("11/17/2018 21:30", "%m/%d/%Y %H:%M"),
-             endtime: DateTime.strptime("11/18/2018 03:30", "%m/%d/%Y %H:%M"),
+             starttime: DateTime.strptime("12/17/2018 21:30", "%m/%d/%Y %H:%M"),
+             endtime: DateTime.strptime("12/18/2018 03:30", "%m/%d/%Y %H:%M"),
              image: Rails.root.join("public/uploads/event/image/3/44460247_864535483670025_3741332332074762240_o.jpg").open,
              user_id: 3)
 
@@ -62,8 +62,8 @@ Event.create(name: "Sunset Pasar & Disco 2.0",
              DRINK and stay hydrated with Canvas Club! Pick your poison with HAPPY HOUR ALL DAY!!
 
              PETS ARE WELCOME AND ADMISSION IS FREE!",
-             starttime: DateTime.strptime("11/17/2018 13:00", "%m/%d/%Y %H:%M"),
-             endtime: DateTime.strptime("11/17/2018 21:00", "%m/%d/%Y %H:%M"),
+             starttime: DateTime.strptime("12/17/2018 13:00", "%m/%d/%Y %H:%M"),
+             endtime: DateTime.strptime("12/17/2018 21:00", "%m/%d/%Y %H:%M"),
              image: Rails.root.join("public/uploads/event/image/4/Sunset Pasar.png").open,
              user_id: 4)
 
@@ -75,8 +75,8 @@ Event.create(name: "Sunday Social Market",
             The Sunday Social Market is a passion project of DJ Zig Zach and Jade Amar, with two very different backgrounds: Music and Fashion. The idea was to create a space where people can hang out with their friends, listen to good music, eat delicious food and discover local brands and talented crafters. All the things they love in one place!
 
             Expect some good ol' House music, artisanal bakers and crafters, interior decor, fashion accessories and of course the hearty roasts from Camp Kilo Charcoal Club.",
-             starttime: DateTime.strptime("11/18/2018 12:00", "%m/%d/%Y %H:%M"),
-             endtime: DateTime.strptime("11/18/2018 22:00", "%m/%d/%Y %H:%M"),
+             starttime: DateTime.strptime("12/18/2018 12:00", "%m/%d/%Y %H:%M"),
+             endtime: DateTime.strptime("12/18/2018 22:00", "%m/%d/%Y %H:%M"),
              image: Rails.root.join("public/uploads/event/image/5/43122710_701850096854936_2521396504288034816_o.jpg").open,
              user_id: 1)
 
@@ -100,8 +100,8 @@ Event.create(name: "Crowd CTRL Presents LeNERD & Sayvere with DJ Rey & TMO",
             A side project pushing new music to the masses by blending timeless commercial hits with trendy remixes of their own coupled with high energy MC-ing & crowd interaction.
 
             THIS EVENT IS 21+",
-             starttime: DateTime.strptime("11/17/2018 22:00", "%m/%d/%Y %H:%M"),
-             endtime: DateTime.strptime("11/18/2018 04:00", "%m/%d/%Y %H:%M"),
+             starttime: DateTime.strptime("12/17/2018 22:00", "%m/%d/%Y %H:%M"),
+             endtime: DateTime.strptime("12/18/2018 04:00", "%m/%d/%Y %H:%M"),
              image: Rails.root.join("public/uploads/event/image/6/44588065_1730240497104947_7278995545446678528_o.jpg").open,
              user_id: 5)
 
@@ -121,8 +121,8 @@ Event.create(name: "Do The Hustle",
             > Cover charge of $18 applies after 10pm,inclusive of 1 standard house pour drink.
             > Dress code is smart casual (no sandals and singlets for men; flip flops, beach and sports wear for all) after 9.30pm on club nights.
             ",
-             starttime: DateTime.strptime("11/17/2018 21:00", "%m/%d/%Y %H:%M"),
-             endtime: DateTime.strptime("11/18/2018 03:00", "%m/%d/%Y %H:%M"),
+             starttime: DateTime.strptime("12/17/2018 21:00", "%m/%d/%Y %H:%M"),
+             endtime: DateTime.strptime("12/18/2018 03:00", "%m/%d/%Y %H:%M"),
              image: Rails.root.join("public/uploads/event/image/7/38267262_644529529266107_5795697081892995072_o.jpg").open,
              user_id: 6)
 
@@ -159,8 +159,8 @@ Event.create(name: "The Foundation presents Dicky Trisco",
             Enough words.
             .
             LET’S DANCE!!!",
-             starttime: DateTime.strptime("11/09/2018 20:00", "%m/%d/%Y %H:%M"),
-             endtime: DateTime.strptime("11/10/2018 02:00", "%m/%d/%Y %H:%M"),
+             starttime: DateTime.strptime("12/09/2018 20:00", "%m/%d/%Y %H:%M"),
+             endtime: DateTime.strptime("12/10/2018 02:00", "%m/%d/%Y %H:%M"),
              image: Rails.root.join("public/uploads/event/image/9/44629765_281202925833610_9099842781647470592_o.jpg").open,
              user_id: 8)
 
@@ -173,8 +173,8 @@ Event.create(name: "Sundown Sundays",
 
             Soundtracked by laidback, island-inspired sounds, enjoy FREE FLOW of house pour spirits, Aperol/Campari spritzers, and chicken skewers, grilled and served a la minute. All yours to the tune of only $58++ per person!
             ",
-             starttime: DateTime.strptime("11/11/2018 16:00", "%m/%d/%Y %H:%M"),
-             endtime: DateTime.strptime("11/11/2018 10:00", "%m/%d/%Y %H:%M"),
+             starttime: DateTime.strptime("12/11/2018 16:00", "%m/%d/%Y %H:%M"),
+             endtime: DateTime.strptime("12/11/2018 10:00", "%m/%d/%Y %H:%M"),
              image: Rails.root.join("public/uploads/event/image/10/37786612_1328418267260611_1205245544985788416_o.jpg").open,
              user_id: 8)
 
@@ -185,8 +185,8 @@ Event.create(name: "Council Saturdays: Haan, Julien Brochard, Satish K & Sivanes
 
             Upstairs sees Satish K and Sivanesh pair up to send the dance floor into a hypnotic stomp with their deep, spanking house grooves.
             ",
-             starttime: DateTime.strptime("11/17/2018 22:00", "%m/%d/%Y %H:%M"),
-             endtime: DateTime.strptime("11/18/2018 04:00", "%m/%d/%Y %H:%M"),
+             starttime: DateTime.strptime("12/17/2018 22:00", "%m/%d/%Y %H:%M"),
+             endtime: DateTime.strptime("12/18/2018 04:00", "%m/%d/%Y %H:%M"),
              image: Rails.root.join("public/uploads/event/image/11/44738544_686057981781023_6801026824261337088_n.jpg").open,
              user_id: 9)
 
@@ -201,8 +201,8 @@ Event.create(name: "Transition ft Menno De Jong",
 
             As a result of this strain of successful releases Menno was asked to become A&R manager of In Trance We Trust in 2014, becoming the face of a label with one of the richest histories in the genre and providing a platform for future Trance stars alike.
             ",
-             starttime: DateTime.strptime("11/17/2018 22:00", "%m/%d/%Y %H:%M"),
-             endtime: DateTime.strptime("11/18/2018 04:00", "%m/%d/%Y %H:%M"),
+             starttime: DateTime.strptime("12/17/2018 22:00", "%m/%d/%Y %H:%M"),
+             endtime: DateTime.strptime("12/18/2018 04:00", "%m/%d/%Y %H:%M"),
              image: Rails.root.join("public/uploads/event/image/12/mennode.png").open,
              user_id: 4)
 
@@ -214,8 +214,8 @@ Event.create(name: "Council Mondays: Deen, Haan, Cats On Crack & Maurice Simon",
 
             Meanwhile, back in the cage at Headquarters, techno underdog Haan and DEEN step up to dish out skull-splitting kickdrums, earth-shaking basslines and distorted synths all night long.
             ",
-             starttime: DateTime.strptime("11/05/2018 21:00", "%m/%d/%Y %H:%M"),
-             endtime: DateTime.strptime("11/06/2018 01:00", "%m/%d/%Y %H:%M"),
+             starttime: DateTime.strptime("12/05/2018 21:00", "%m/%d/%Y %H:%M"),
+             endtime: DateTime.strptime("12/06/2018 01:00", "%m/%d/%Y %H:%M"),
              image: Rails.root.join("public/uploads/event/image/14/43725924_686042068449281_4148750773699739648_n.jpg").open,
              user_id: 9)
 
